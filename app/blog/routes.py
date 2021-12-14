e3c6a65babb9a95fc92e9cbf9c0e80402aa1ce52
ashernaphtali <aaboucher@gmail.com>
2021-12-14 13:25:09 -0500

    week 6 day 1 homework updates'

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -10,10 +10,21 @@ blog = Blueprint('blog', __name__, template_folder='blog_templates')
 
 from app.models import db
 
+import requests as r
+
 @blog.route('/')
 def blogHome():
     posts = Post.query.all()
     return render_template('blog.html', posts = posts)
+
+
+@blog.route('/pokebikes')
+def pokeBikes():
+    data = r.get('https://pokeapi.co/api/v2/item/bicycle/')
+    my_data = data.json()
+    bikes = [dict['name'] for dict in my_data['names']]
+    my_img = my_data['sprites']['default']
+    return render_template('pokebikes.html',bikes=bikes, my_img=my_img)
 
 @blog.route('/posts/create', methods = ["GET","POST"])
 @login_required
@@ -35,3 +46,29 @@ def createPost():
 
             return redirect(url_for('home'))
     return render_template('create_post.html', form = form)
+
+
+@blog.route('/search/pokemon', methods = ['POST'])
+def searchPokemon():
+    if request.method =="POST":
+        my_pokemon = request.form['poke']
+        data = r.get(f'https://pokeapi.co/api/v2/pokemon/{my_pokemon}')
+        if data.status_code == 200:
+            my_data = data.json()
+
+            pokemon = {
+                'name': '',
+                'image': '',
+                'abilities': []
+            }
+
+            for ability in my_data['abilities']:
+                pokemon['abilities'].append(ability['ability']['name'])
+            pokemon['name'] = my_data['name']
+            pokemon['image'] = my_data['sprites']['front_default']
+
+            return render_template('pokemon.html', pokemon = pokemon)
+        else:
+            pokemon = ''
+            return render_template('pokemon.html', pokemon = pokemon)
+    return {'hi': 'there'}
